dece0e7f1d6a993026f2787d4f53a30b83156ba4
Astrixx <121934719+Astrixx9061@users.noreply.github.com>
2023-06-23 00:18:28 +0300

    Add files via upload

---
+++ Airport/Airport-Azis2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Azis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = "1";
+            string date;
+            int svobodnimesta;
+            int countpurchasedtickets;
+            Console.Write("Коя дата сме? ");
+            date = Console.ReadLine();
+            Console.Write("Колко свободни места има? ");
+            svobodnimesta = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Колко закупени билета има? ");
+            countpurchasedtickets = int.Parse(Console.ReadLine());
+            Console.WriteLine("Данни:");
+            Console.WriteLine("     Код на полета: "+code);
+            Console.WriteLine("     Дата: "+ date);
+            Console.WriteLine("     Свободни места: " + svobodnimesta);
+            Console.WriteLine("     Закупени билети: " + countpurchasedtickets);
+
+
+
+
+
+
+
+        }
+    }
+}
